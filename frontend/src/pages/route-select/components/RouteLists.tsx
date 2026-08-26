35d7c984da8c0cf74c297e6e17e80b720f196cfe
bigcloud07 <dnsxo0712@gmail.com>
2026-08-26 18:32:30 +0900

    feat(SAL-71): RouteList 컴포넌트 추가

--- frontend/src/pages/route-select/components/RouteLists.tsx
+++ frontend/src/pages/route-select/components/RouteLists.tsx
@@ -1,3 +1,20 @@
-export function RouteLists() {
-  return <></>;
+import type { RouteCore } from "../api/RouteSelect.type";
+import { RouteListItem } from "./RouteListItem";
+
+interface RouteListProps {
+  routes: RouteCore[];
+}
+
+export function RouteList({ routes }: RouteListProps) {
+  return (
+    <ul>
+      {routes.map((route) => (
+        <RouteListItem
+          displayName={route.displayName}
+          startStopName={route.startStopName}
+          endStopName={route.endStopName}
+        />
+      ))}
+    </ul>
+  );
 }
