cff9632acd39a18e9349f6459745244ab1d9917e
bigcloud07 <dnsxo0712@gmail.com>
2026-08-28 00:18:04 +0900

    chore(SAL-71): App.css 파일 삭제로 인한 경로 제거 및 className 제거

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,8 @@
-import * as styles from "./App.css";
 import { RouteSelectPage } from "@/pages/route-select/RouteSelectPage";
 
 export function App() {
   return (
-    <main className={styles.layout}>
+    <main>
       <RouteSelectPage />
     </main>
   );
