1dc470fba4b35141efdd0f77a4787d96690cf4f6
lee-eojin <lejin0906@snu.ac.kr>
2026-09-06 16:18:52 +0900

    chore: dev 프록시 대상을 env 파일에서 읽도록 설정 (API_PROXY_TARGET)

--- frontend/webpack/webpack.dev.cjs
+++ frontend/webpack/webpack.dev.cjs
@@ -3,6 +3,13 @@ const { merge } = require("webpack-merge");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const common = require("./webpack.common.cjs");
 
+// 아래 devServer.proxy가 설정 시점에 process.env를 읽는다. webpack의 dotenv 옵션은 번들에만 값을 넣는다.
+try {
+  process.loadEnvFile(path.resolve(__dirname, "../env/.env.development"));
+} catch {
+  // 파일이 없으면 기본값으로 간다
+}
+
 module.exports = merge(common, {
   mode: "development",
   dotenv: {
@@ -38,7 +45,7 @@ module.exports = merge(common, {
     proxy: [
       {
         context: ["/api"],
-        target: process.env.API_PROXY_TARGET ?? "http://localhost:8080",
+        target: process.env.API_PROXY_TARGET || "http://localhost:8080",
         changeOrigin: true,
       },
     ],
