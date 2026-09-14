92f36cadc09f9b0c74f5815f7a31617ddeda9835
bigcloud07 <dnsxo0712@gmail.com>
2026-09-14 21:05:47 +0900

    chore(SAL-126): storybook 의존성 추가

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
+  ],
+  "framework": "@storybook/react-webpack5"
+};
+export default config;
